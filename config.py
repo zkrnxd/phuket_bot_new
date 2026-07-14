bbcdc877821eff670d8f8fa986956d917e0fd586
Asssrkx <zakrevikgame@gmail.com>
2026-07-14 18:08:04 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,12 @@
-EXCEL_PATH = "data/Thailand_Bot_Master_Districts_filled-1.xlsx"
+import os
+
+# Токен берем из переменных окружения
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+ACCESS_CODE = os.getenv("ACCESS_CODE", "PHUKET2026")
+
+# Если токен не найден — ошибка
+if not BOT_TOKEN:
+    raise ValueError("❌ BOT_TOKEN не найден! Добавьте переменную BOT_TOKEN")
+
+# Путь к Excel файлу
+EXCEL_PATH = "data/Thailand_Bot_Master_Districts_filled.xlsx"
