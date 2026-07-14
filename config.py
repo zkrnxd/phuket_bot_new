f817792ac366d15ed4b9a5663239021c04f817ab
Asssrkx <zakrevikgame@gmail.com>
2026-07-14 18:03:09 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,1 @@
-# ТОКЕН БОТА (получили от @BotFather)
-BOT_TOKEN = "8785132524:AAEWpHFcz5P3vtavEgPkKKC6pchLlvDxb14"
-
-# Путь к Excel файлу
 EXCEL_PATH = "data/Thailand_Bot_Master_Districts_filled-1.xlsx"
-
-# Пароль для доступа к боту
-ACCESS_CODE = "PHUKET2026"
